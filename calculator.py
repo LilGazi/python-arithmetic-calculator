09fd93d28aa845e7b47bdb941ad95fab9290975e
LilGazi <godslovemarcelo@gmail.com>
2026-08-12 00:37:30 +0100

    Implement basic arithmetic operations in calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,44 @@
+def add(x, y):
+    return x + y
 
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+calculate = input("Enter arithmetic operation +, -, *, /: ")
+
+if calculate == "+":
+    x = float(input("Enter number: "))
+    y = float(input("Enter number: "))
+    result = add(x, y)
+    print("Result:", result)
+
+elif calculate == "-":
+    x = float(input("Enter number: "))
+    y = float(input("Enter number: "))
+    result = subtract(x, y)
+    print("Result:", result)
+
+elif calculate == "*":
+    x = float(input("Enter number: "))
+    y = float(input("Enter number: "))
+    result = multiply(x, y)
+    print("Result:", result)
+
+elif calculate == "/":
+    x = float(input("Enter number: "))
+    y = float(input("Enter number: "))
+    result = divide(x, y)
+    print("Result:", result)
+
+else:
+    print("Invalid operation.")
